46d9265b8fbb3d24efd088478aa00f4611f6e1b6
mohammadreza booshehri <mrbooshehri@gmail.com>
2020-12-19 22:47:30 +0300

    ~/.dwm added

---
+++ .dwm/autostart.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+# status bar
+#dwmblocks &
+
+# transparancy
+compton -f -D 1 &
+
+# set layout
+setxkbmap -layout us,ir 
+setxkbmap -option 'grp:alt_shift_toggle' 
+
+# conky
+conky &
+
+# redshift
+redshift -c ~/.config/redshift/redshift.conf &
+
+# application
+korganizer &
+nm-applet &
+blueman-applet &
+variety &
+
