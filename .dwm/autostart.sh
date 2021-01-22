abaee9feb85c8210bc05feea6307e1afc72acdbc
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-01-22 22:16:18 +0300

    pol-kit and conky check added

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -10,8 +10,11 @@ compton -f -D 1 &
 setxkbmap -layout us,ir 
 setxkbmap -option 'grp:alt_shift_toggle' 
 
+# pol-kit
+/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
+
 # conky
-conky &
+conky > /dev/null 2>&1 &
 
 # redshift
 redshift -c ~/.config/redshift/redshift.conf > /dev/null 2>&1 &
